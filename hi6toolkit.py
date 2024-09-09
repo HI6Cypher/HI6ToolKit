0f7fcf9de8a96e6f406ec3d6379b01f9e51d91a9
hi6cypher <huaweisclu31@hotmail.com>
2024-09-09 22:54:35 +0330

    forgot add sock.settimeout(self.timeout) to Tunnel.init_server method

--- hi6toolkit.py
+++ hi6toolkit.py
@@ -404,6 +404,7 @@ class Tunnel :
 
     def init_server(self) :
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.settimeout(self.timeout)
         self.sock.bind((self.host, self.port))
         self.sock.listen(1)
         return None
